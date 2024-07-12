2ddf64f5ae8e2198a1ffa0b8b10107ccfa23992c
Delphin008 <praktikum@ffw.com>
2024-07-12 14:14:45 +0200

    Save game to ram (desktop)

--- composeApp/src/desktopMain/kotlin/data/GameRepository.desktop.kt
+++ composeApp/src/desktopMain/kotlin/data/GameRepository.desktop.kt
@@ -3,15 +3,16 @@ package data
 import GameState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import util.gelds
 
 actual class GameRepositoryImpl actual constructor() : GameRepository {
 
-    private var _memory: GameState = GameState(0.gelds, emptyList())
+    private var _memory: MutableStateFlow<GameState> = MutableStateFlow(GameState(0.gelds, emptyList()))
 
-    override fun getGame(): Flow<GameState> = MutableStateFlow(_memory)
+    override fun getGame(): Flow<GameState> = _memory
 
     override fun saveGame(gameState: GameState) {
-        _memory = gameState
+        _memory.update { gameState }
     }
 }
